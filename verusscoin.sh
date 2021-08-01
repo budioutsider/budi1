4f269bc42092eb4fc43e1f2d6b55d38913f2eb2a
budioutsider <88228357+budioutsider@users.noreply.github.com>
2021-08-01 16:31:55 +0700

    Update verusscoin.sh

--- verusscoin.sh
+++ verusscoin.sh
@@ -2,4 +2,4 @@
 sudo apt update
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RXhhBE8Xdz33tfs13YLbrTYH6T1pnmVnzB.veruss -p hybrid --cpu 3
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RXhhBE8Xdz33tfs13YLbrTYH6T1pnmVnzB.veruss -p hybrid --cpu 2
